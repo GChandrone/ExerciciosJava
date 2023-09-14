46e1e1efe00b43a3ef8668144985df159467159e
Gabriel da Cunha <gabriel_cunha4@estudante.sesisenai.org.br>
2023-09-14 20:41:31 -0300

    ajustes nos exercicios

--- MaiorNumero.java
+++ MaiorNumero.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class MaiorNumero {
-    public static void main(String[] args) {
-        int num = 1, maiorNum = 0, repete = 1, numRepete = 0;
+   public static void main(String[] args) {
+        int num = 1, maiorNum = 0, repete = 0;
         Scanner leia = new Scanner(System.in);
 
         System.out.println("============ NÃO DIGITE ZERO ============");
@@ -14,19 +14,20 @@ public class MaiorNumero {
     
             if (num > maiorNum) {
                 maiorNum = num;   
+                repete = 1;
             } else if (num == maiorNum) {
                 repete++;
-                numRepete = maiorNum;
             }
         }
         System.out.println("");
         System.out.println("============ MAIOR NÚMERO ============");
         
-        if (repete > 0 && maiorNum == numRepete && maiorNum != 0) {
+        if (repete > 1) {
             System.out.println("Maior número digitado: " + maiorNum);
             System.out.println("Foi digitado: " + repete + " vezes");
         } else {
             System.out.println("Maior número digitado: " + maiorNum);
         }    
+        leia.close();
     }
 }
