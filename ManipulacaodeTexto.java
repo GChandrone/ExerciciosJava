a0665dd733078f35f648746bb318c1f99c72a40b
Gabriel da Cunha <gabriel.cunhagabi@gmail.com>
2023-09-20 23:56:38 -0300

    Java - indexOf

---
+++ ManipulacaodeTexto.java
@@ -0,0 +1,7 @@
+public class ManipulacaodeTexto {
+    public static void main(String[] args) {
+        /* indexOf retorna o índice (posição) da palavra que quer encontrar */
+        String txt = "Por favor localize onde ocorre localize!";
+        System.out.println("A palavra \"localize\" começa na posição: " + txt.indexOf("localize "));
+    }
+}
