a309949c6dae0119e7a73c4e17ce04bb0164c12d
Gabriel da Cunha <gabriel.cunhagabi@gmail.com>
2023-10-04 21:50:01 -0300

    organizando pastas e adicionando atividade

---
+++ Ex_10deOutubro.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex_10deOutubro {
+    public static void main(String[] args) {
+        int[] Vet = new int[50];
+
+        Scanner leia = new Scanner(System.in);
+
+        for (int i = 0; i < Vet.length; i++) {
+            System.out.print("Digite o número inteiro da posição " + (i+1) + " : ");
+            Vet[i] = leia.nextInt();
+            
+            System.out.println(""); 
+            
+            for (int c = 0; c < Vet.length; c++) {
+                if (Vet[i] == Vet[c] && i != c) {
+                    System.out.println("-----------------------------------------");
+                    System.out.println("O número " + Vet[i] + " é repetido!!");
+                    System.out.println("-----------------------------------------");
+                    System.out.println("");
+                    break;
+                }
+            }
+        }
+        leia.close();
+    }
+}
